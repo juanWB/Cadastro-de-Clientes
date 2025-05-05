0a06ac6386c1b7818da08604e0db1ad3ceaee7cc
Juan Alejandro <juan.acv.06@gmail.com>
2025-05-05 17:44:31 -0300

    Implementação das queries para trazer, atualizar  e deletar clientes.

--- backend/src/Model/ClientModel.ts
+++ backend/src/Model/ClientModel.ts
@@ -3,9 +3,9 @@ import { TClient } from "../Schema/ClientSchema";
 
 
 export const createClient = async(client: TClient) => {
-    const query = 'INSERT INTO clientes (cnpj, nome, nome_fantasia, cep, logradouro, bairro, cidade, uf, complemento, email, telefone)' + 
-                  'VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)';
-
+    const query = `INSERT INTO clientes (cnpj, nome, nome_fantasia, cep,
+                   logradouro, bairro, cidade, uf, complemento, email, telefone)
+                   VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)`;
     const result = await DbConnection.query(query, [client.cnpj, client.nome, client.nome_fantasia,
                                             client.cep, client.logradouro, client.bairro, client.cidade,
                                             client.uf, client.complemento, client.email, client.telefone]);
@@ -13,3 +13,26 @@ export const createClient = async(client: TClient) => {
 }
 
 
+export const getClients = async() => {
+    const query = `SELECT * FROM clientes`;
+    const [result] = await DbConnection.query(query);
+    return result;
+}
+
+
+export const updateById = async (client: TClient) => {
+    const query = `UPDATE clientes SET nome = ?, nome_fantasia = ?, cep = ?, logradouro = ?, 
+                   bairro = ?, cidade = ?, uf = ?, complemento = ?, email = ?, telefone = ?
+                   WHERE id = ?` ;
+    const [result] = await DbConnection.query(query, [client.nome, client.nome_fantasia,
+                                              client.cep, client.logradouro, client.bairro,
+                                              client.cidade, client.uf, client.complemento,
+                                              client.email, client.telefone, client.id]);
+    return result;
+}
+
+export const deleteById = async(id: number) => {
+    const query = `DELETE FROM clientes WHERE id = ?`
+    const result =  await DbConnection.execute(query, [id]);
+    return result;
+}
