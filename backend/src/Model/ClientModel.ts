0c8130c28e3ed835d995888e1b8e39c658a0ddf9
Juan Alejandro <juan.acv.06@gmail.com>
2025-05-05 17:00:08 -0300

    Implementação da query para criação de um novo cliente.

--- backend/src/Model/ClientModel.ts
+++ backend/src/Model/ClientModel.ts
@@ -1,1 +1,15 @@
 import { DbConnection } from "../DbConfig/DbConfig";
+import { TClient } from "../Schema/ClientSchema";
+
+
+export const createClient = async(client: TClient) => {
+    const query = 'INSERT INTO clientes (cnpj, nome, nome_fantasia, cep, logradouro, bairro, cidade, uf, complemento, email, telefone)' + 
+                  'VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)';
+
+    const result = await DbConnection.query(query, [client.cnpj, client.nome, client.nome_fantasia,
+                                            client.cep, client.logradouro, client.bairro, client.cidade,
+                                            client.uf, client.complemento, client.email, client.telefone]);
+    return result;
+}
+
+
