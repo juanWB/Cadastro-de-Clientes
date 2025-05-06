63a4367df91699d3a52af2ba7e656943e2b5fe26
Juan Alejandro <juan.acv.06@gmail.com>
2025-05-06 15:45:13 -0300

    Criação do tipo cliente.

---
+++ frontend/src/types/ClientType.ts
@@ -0,0 +1,14 @@
+export type TClient = {
+    id: number;
+    cnpj: string;
+    nome: string;
+    nome_fantasia: string;
+    cep: string;
+    logradouro: string;
+    bairro: string;
+    cidade: string;
+    uf: string;
+    complemento: string;
+    email: string;
+    telefone: string;
+};
